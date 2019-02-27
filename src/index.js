4ccbdfc807df593c3b1c6aee4534b6f9e5a9ea29
fshuirong <702017364@qq.com>
2019-02-27 17:46:42 +0800

    调整发布环境

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import noop from './utils/noop.js';
+import array2json from './utils/array2json.js';
+import closureCache from './utils/closure-cache.js';
+import formatDate from './utils/format-date.js';
+import tasksSynch from './utils/tasks-synch.js';
+
+export default {
+  version: '1.0.0',
+  array2json,
+  closureCache,
+  formatDate,
+  noop,
+  tasksSynch,
+};
